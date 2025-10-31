c0681a18e110fd1528be5d575b8bbed57955eb26
Pritesh <priteshbavaskar@gmail.com>
2025-10-31 12:28:47 +0530

    Home Registration info and platform pages done

--- src/sections/home/view/home-view.js
+++ src/sections/home/view/home-view.js
@@ -7,6 +7,8 @@ import HomeHero from '../home-hero';
 import HomeWhyChooseIssuerPro from '../home-why-choose-issuer-pro';
 import HomeGetStarted from '../home-get-started';
 import HomeSupport from '../home-support';
+import HomeRegistrationProcess from '../home-registration-process';
+import HomeUsingPlatform from '../home-using-platform';
 // ----------------------------------------------------------------------
 
 export default function HomeView() {
@@ -18,6 +20,8 @@ export default function HomeView() {
 
       <HomeHero />
       <HomeWhyChooseIssuerPro />
+      <HomeRegistrationProcess />
+      <HomeUsingPlatform />
       <HomeGetStarted />
       <HomeSupport />
     </>
